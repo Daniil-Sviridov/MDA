9ab769322549815b9ad03385fc5a3830ec315f96
Daniil Sviridov <daniil_sviridov@mail.ru>
2022-11-09 17:49:51 +0300

    L3 start

---
+++ Messaging/KitchenReady.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging
+{
+    public class KitchenReady : IKitchenReady
+    {
+        public KitchenReady(Guid orderId, bool ready)
+        {
+            OrderId = orderId;
+            Ready = ready;
+        }
+
+        public Guid OrderId { get; }
+        public bool Ready { get; }
+    }
+}
---
+++ MyRest.Kitchen/Manager.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Messaging;
+
+namespace MyRest.Kitchen
+{
+    internal class Manager
+    {
+        private readonly IBus _bus;
+
+        public Manager(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public void CheckKitchenReady(Guid orderId, Dish? dish)
+        {
+            _bus.Publish<IKitchenReady>(new KitchenReady(orderId, true));
+        }
+    }
+}
